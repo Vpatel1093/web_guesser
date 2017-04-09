9c3c3a4c053ae765d7bf13443ae281bbee783ec0
Vishal <v.patel1093@gmail.com>
2017-04-08 22:41:37 -0400

    I4: Colorized output

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,11 +6,13 @@ SECRET_NUMBER=rand(101)
 get '/' do
 	guess = params["guess"]
 	message = check_guess(guess)
-	erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+	style_color = color_guess(guess)
+	erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :style_color => style_color}
 end
 
 def check_guess(guess)
 	difference = guess.to_i - SECRET_NUMBER
+	
 	if guess == nil
 		""
 	elsif difference > 5
@@ -25,4 +27,18 @@ def check_guess(guess)
 	elsif difference < -5
 		"Way too low!"
 	end	
+end
+
+def color_guess(guess)
+	difference = guess.to_i - SECRET_NUMBER
+	
+	if guess == nil
+		return "white"
+	elsif difference.abs > 5
+		return "red"
+	elsif difference.abs > 0 && difference.abs < 5
+		return "palevioletred"
+	elsif difference == 0
+		return "green"
+	end
 end
